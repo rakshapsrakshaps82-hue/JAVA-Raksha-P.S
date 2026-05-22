0d2e878a88a206ad3f381520476c67dd019ad3b0
Raksha P.S <rakshapsrakshaps@gmail.com>
2026-05-22 11:06:32 +0530

    DAY 1

---
+++ relational.java
@@ -0,0 +1,13 @@
+public class relational {
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        System.out.println("a is greater than b:"+(a>b));
+        System.out.println("a is less than b:"+(a<b));
+        System.out.println("a is equal to b:"+(a==b));
+        System.out.println("a is not equal to b:"+(a!=b));
+        System.out.println("a is greater than or equal to b:"+(a>=b));
+        System.out.println("a is less than or equal to b:"+(a<=b));
+    }
+    
+}
