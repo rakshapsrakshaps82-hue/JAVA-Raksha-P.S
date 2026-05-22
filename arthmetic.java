4cf02d9af9faafa9df00bd769783ae031829b23d
Raksha P.S <rakshapsrakshaps@gmail.com>
2026-05-22 10:49:04 +0530

    DAY-1

---
+++ arthmetic.java
@@ -0,0 +1,16 @@
+public class arthmetic{
+    public static void main (String[]args){
+        int a=10;
+        int b=20;
+        int sum=a+b;
+        int diff=a-b;
+        int mul=a*b;
+        float div=a/b;
+        System.out.println("The sum of a and b is:"+sum);
+        System.out.println("The difference of a and b is:"+diff);
+        System.out.println("The product of a and b is:"+mul);
+        System.out.println("The division of a and b is:"+div);
+    }
+}
+    
+
