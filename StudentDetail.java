18e3735bd5a9f06d90043a22570a366d315f52f2
Raksha P.S <rakshapsrakshaps@gmail.com>
2026-05-24 21:58:30 +0530

    TASK 1

---
+++ StudentDetail.java
@@ -0,0 +1,15 @@
+public class StudentDetail {
+    public static void main(String[] args){
+        String name="RAKSHA P.S";
+        int age=20;
+        String course="Computer Science";
+        String university="VTU";
+        String USN="1ME24CS060";
+        System.out.println("Student Name: "+name);
+        System.out.println("Student Age: "+age);
+        System.out.println("Student Course: "+course);
+        System.out.println("Student University: "+university);
+        System.out.println("Student USN: "+USN);
+    }
+    
+}
